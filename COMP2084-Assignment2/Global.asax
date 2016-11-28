59c4e4876464f8da1853f1ce1bd385e9de11120d
tlherr <thom1810@gmail.com>
2016-11-28 17:18:01 -0500

    Initial Commit

---
+++ COMP2084-Assignment2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="COMP2084_Assignment2.MvcApplication" Language="C#" %>
---
+++ COMP2084-Assignment2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(COMP2084_Assignment2.Startup))]
+namespace COMP2084_Assignment2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
