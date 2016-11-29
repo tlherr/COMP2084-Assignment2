6954102898483de04c4f3dfdf07fc83b7c895442
tlherr <thom1810@gmail.com>
2016-11-29 08:07:04 -0500

    Updated controllers to no longer have default application names

---
+++ COMP2084-Assignment2/Controllers/MatchController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COMP2084_Assignment2.Controllers
+{
+    public class MatchController : Controller
+    {
+        // GET: Match
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
