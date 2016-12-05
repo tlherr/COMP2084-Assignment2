6486c99bf0fe4bf9471fa9150233e9db81f95756
Thomas Herr <thom1810@gmail.com>
2016-12-05 16:06:48 -0500

    Started changing layout to be more customized

--- COMP2084-Assignment2/Controllers/HomeController.cs
+++ COMP2084-Assignment2/Controllers/HomeController.cs
@@ -12,19 +12,5 @@ namespace COMP2084_Assignment2.Controllers
         {
             return View();
         }
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
