9e4b96dfb3a42fe1a0159699e57953f4bb701762
Thomas Herr <thom1810@gmail.com>
2016-12-12 13:33:49 -0500

    Added custom 500 and 404 pages.

---
+++ COMP2084-Assignment2/Controllers/ErrorController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace COMP2084_Assignment2.Controllers
+{
+    public class ErrorController : Controller
+    {
+        // GET: Error/NotFound
+        public ActionResult NotFound()
+        {
+            Response.StatusCode = 404;
+            return View();
+        }
+        // GET: Error/Error
+        public ActionResult Error()
+        {
+            Response.StatusCode = 500;
+            return View();
+        }
+    }
+}
